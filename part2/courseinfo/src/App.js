4510108e80a98e8d260318b4d6c2b0da15966ced
Brian Ballsun-Stanton <brian@fedarch.org>
2020-08-26 16:21:06 +1000

    refactor out Course, 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,54 +1,6 @@
 import React from 'react'
+import Course from './components/Course'
 
-// It's kinda silly moving all of this from index into App, but I suspect we're going to
-// refactor at some point, and I should probably get the assignment done first.
-//
-
-const Course = ({course}) => {
-
-  return (
-    <div>
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course}/>
-    </div>
-  )
-}
-
-const Total = (props) => {
-    const sumExercises =
-      props.course.parts.reduce((totalExercisesSum, exercise) =>
-        totalExercisesSum + exercise['exercises'], 0
-      ) //Don't forget that trailing 0, which looks like a getter with default value. Also, don't need to map out the variable first. Hard getting my head around this lambda function, but ok... yeah, I see it.
-
-    return(
-		<p> <b>total of {sumExercises}  exercises</b> </p>
-	)
-}
-const Header = (props) => (
-	<h1>{props.course.name}</h1>
-)
-
-const Part = (props) => (
-	<div>
-		<p>{props.part['name']} {props.part['exercises']}</p>
-	</div>
-)
-
-const Content = (props) => {
-	console.log(props)
-	const PartsMap = props.course.parts.map(value =>
-		<Part key={value.id} part={value}/>
-    // Hah, and this is why we don't prematurely optimise. It's almost like this unit was designed with this in mind...
-	)
-	return (
-		<div>
-
-			{PartsMap}
-
-		</div>
-	)
-}
 
 
 const App = () => {
@@ -106,4 +58,4 @@ const App = () => {
 
 
 
-export default App;
+export default App
