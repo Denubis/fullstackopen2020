a50bc5265bbef0e87b51c35c7e6f2f0a3c16b9d1
Brian Ballsun-Stanton <brian@fedarch.org>
2020-09-02 17:34:39 +1000

    2.9 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,28 @@
 import React, {useState} from "react";
 const Name = ({name}) => (<div>{name.name} {name.number}</div>)
+const NameList = ({filterPersons}) => {
+
+  if (filterPersons.length > 0) {
+    return (filterPersons.map(person => <Name key={person.name} name={person}/>))
+  } else {
+    return (<div>No people found</div>)
+  }
+}
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: "Arto Hellas",
-      number: "040-1234567"
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
-  const [newName, setNewName] = useState(""); // We need a name change handler.
-  const [newNumber, setNewNumber] = useState(""); // We need a name change handler.
-
+  const [newName, setNewName] = useState("") // We need a name change handler.
+  const [newNumber, setNewNumber] = useState("") // We need a name change handler.
+  const [filterName, setNewFilterName] = useState("")
   const handleNameChange = (event) => { setNewName(event.target.value)}
   const handleNumberChange = (event) => { setNewNumber(event.target.value)}
-
+  const handleFilterChange = (event) => {
+    setNewFilterName(event.target.value)
+  }
 const addPerson = (event) => {
   event.preventDefault()
   console.log('nameclick', event.target)
@@ -30,8 +40,18 @@ const addPerson = (event) => {
   setNewName('')
   setNewNumber('')
 }
+
+const filterPersons = filterName
+? persons.filter((cur) => (cur.name.includes(filterName)))
+: persons
+
+console.log(filterPersons)
+
+
 return (<div>
   <h2>Phonebook</h2>
+  filter shown with <input value={filterName} onChange={handleFilterChange}/>
+  <h2>Add a new</h2>
   <form onSubmit={addPerson}>
     <div>
       name:
@@ -47,7 +67,11 @@ return (<div>
     </div>
   </form>
   <h2>Numbers</h2>
-  {persons.map(person => <Name key={person.name} name={person}/>)}
-</div>);
-};
+
+
+    <NameList filterPersons={filterPersons}/>
+
+</div>)
+}
+
 export default App;
