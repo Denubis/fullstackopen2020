33d34158436144f89f132201b5abc686eb2b5ad6
Brian Ballsun-Stanton <brian.ballsun-stanton@mq.edu.au>
2020-09-17 16:01:58 +1000

    2.14 done

--- part2/countries/src/components/CountryDetails.js
+++ part2/countries/src/components/CountryDetails.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios'
 
+
+
+
 const CountryDetails = ({result}) => {
+
+  const REACT_APP_WEATHER_QUERY = `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_WEATHER_API_KEY}&query=${result.capital}`
+
+  const [weather, setWeather] = useState([])
+
+  const hook = () => {
+    console.log("Rest query for", REACT_APP_WEATHER_QUERY)
+    axios
+      .get(REACT_APP_WEATHER_QUERY)
+      .then(response => {
+        console.log("weather Results", response.data.current)
+        setWeather(response.data.current)
+      })
+
+  }
+  useEffect(hook, []) // load the data once, then do local processing.
+
+
   return (
   <div>
     <h2>{result.name}</h2>
@@ -12,6 +33,10 @@ const CountryDetails = ({result}) => {
       {result.languages.map(language => (<li key={language.iso639_1}>{language.name}</li>))}
     </ul>
     <img src={result.flag} width="25%" alt={result.name} />
+    <h3>Weather in {result.capital}</h3>
+    <div>Temperature: {weather.temperature} C</div>
+    <div><img src={weather.weather_icons} alt={weather.weather_icons}/></div>
+    <div>Wind: {weather.wind_speed} units unknown direction {weather.wind_dir}</div>
   </div>
 )}
 
